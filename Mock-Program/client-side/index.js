68f408aa65a7d770b167873da4c5ada0cf55f81c
chris-cozy <csande9@clemson.edu>
2022-10-13 22:43:18 -0400

    Implemented client-side of mock program

---
+++ Mock-Program/client-side/index.js
@@ -0,0 +1,6 @@
+/*
+    This is an event listener that is called when the page loads. It fetches the endpoint for table data.
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Test')
+});
